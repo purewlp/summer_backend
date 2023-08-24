decb0e3a64a673f63a758bfa05038a5d2840395e
qcloud <ubuntu@localhost.localdomain>
2023-08-24 16:04:38 +0800

    db

--- Platform/Platform/settings.py
+++ Platform/Platform/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = "Platform.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'backend',
+        'USER':'root',
+        'PASSWORD':'Wlp123456789',
+        'HOST':'bj-cynosdbmysql-grp-kre0sm36.sql.tencentcdb.com',
+        'PORT':29763,
     }
 }
 
