675a1a87987b876921ba7515b71e250ef0436373
qcloud <ubuntu@localhost.localdomain>
2023-08-24 12:45:14 +0800

    test

--- Platform/Platform/settings.py
+++ Platform/Platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-g)_!-atlm5$p^d4^1yl5j5!7)u3s*^)_#rm)8htondpptiqs1*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,19 +37,48 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'user',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 ROOT_URLCONF = "Platform.urls"
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
+CORS_ALLOW_CREDENTIALS = True  # 允许携带Cookie
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
--- Platform/user/views.py
+++ Platform/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from Platform import settings
 # Create your views here.
+
+def test(request):
+    if request.method=='POST':
+        return JsonResponse({'errno':0,'msg':"测试成功"})
+    else:
+        return JsonResponse({'errno':1001,'msg':"测试失败"})
