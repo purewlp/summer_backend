12dbbbb2b4d7a79f1d584cdd898e4cfdb6c3b619
whytomato <1158152324@qq.com>
2023-08-26 11:10:51 +0800

    项目管理

--- Platform/project/models.py
+++ Platform/project/models.py
@@ -26,8 +26,20 @@ class Project(models.Model):
 
 class ProjectRecycleBin(models.Model):
     name=models.CharField(max_length=100)
-    creator=models.ForeignKey(User,on_delete=models.CASCADE)
-    team=models.ForeignKey(Team,on_delete=models.CASCADE)
+    deleter_id=models.BigIntegerField(null=True)
+    creator_id=models.BigIntegerField(null=True)
+    team=models.ForeignKey(Team,on_delete=models.CASCADE,null=True)
+    project_id=models.BigIntegerField(null=True)
+
+    def save(self, *args, **kwargs):
+        # 如果项目ID尚未分配，分配一个从10001开始的唯一项目ID
+        if self.id is None:
+            last_project = ProjectRecycleBin.objects.order_by('-id').first()
+            if last_project:
+                self.id = last_project.id + 1
+            else:
+                self.id = 10001
+        super().save(*args,**kwargs)
 
     def __str__(self):
         return self.name
--- Platform/project/urls.py
+++ Platform/project/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns=[
     path('create',createProject),
-    
+    path('delete',deleteProject),
+    path('recover',recoverProject),
+    path('rename',renameProject),
+    path('list/',list),
 ]
