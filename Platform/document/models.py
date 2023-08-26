9e9719824706059700947c5da2bab5d57eab7671
whytomato <1158152324@qq.com>
2023-08-27 01:55:20 +0800

    创建文档

--- Platform/document/models.py
+++ Platform/document/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from project.models import Project
 
 # Create your models here.
+class Document(models.Model):
+    project=models.ForeignKey(Project,on_delete=models.CASCADE,null=True)
+    name=models.CharField(max_length=20,null=True,default="新建文档")
+
+    def __str__(self):
+        return self.name
+    def save(self, *args, **kwargs):
+        if self.id is None:
+            # 如果 ID 为空，为其分配一个从 1 开始的值
+            last_document = Document.objects.order_by('-id').first()
+            if last_document:
+                self.id = last_document.id + 1
+            else:
+                self.id = 1
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        db_table='document'
--- Platform/document/views.py
+++ Platform/document/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from project.models import Project
+from document.models import Document
+from Platform import settings
+from django.http import JsonResponse
 
 # Create your views here.
+
+def create(request):
+    if request.method == 'POST':
+        projectID=request.POST.get('project_id')
+        project=Project.objects.get(id=projectID)
+        name=request.POST.get('name')
+        Document.objects.create(project=project)
+        return JsonResponse({'errno':0})
+    else:
+        return JsonResponse({'errno':1001})
