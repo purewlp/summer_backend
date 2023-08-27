e072770023b7e6620f845ccc559ccb691bcc6608
wlp <112996920+purewlp@users.noreply.github.com>
2023-08-27 16:30:00 +0800

    post更改

--- Platform/chat/views.py
+++ Platform/chat/views.py
@@ -13,13 +13,11 @@ from chat.models import Room, ChatMessage, UserRoom, Document
 class RoomView(View):
     # 创建聊天室
     def post(self, request: HttpRequest):
-        json_obj = json.loads(request.body)
-        # 检验字段是否完整
         try:
-            members = json_obj['members']
-            teamId = json_obj['teamId']
-            roomName = json_obj['roomName']
-            userId = json_obj['userId']
+            members = request.POST.get('members')
+            teamId = request.POST.get('teamId')
+            roomName = request.POST.get('roomName')
+            userId = request.POST.get('userId')
         except:
             return HttpResponse({"status":400})
 
@@ -51,10 +49,10 @@ class MessageView(View):
     # 聊天室历史消息
 
     def get(self, request: HttpRequest):
-        json_obj = json.loads(request.body)
+
         try:
-            roomId = json_obj['roomId']
-            userId = json_obj['userId']
+            roomId = request.POST.get('roomId')
+            userId = request.POST.get('userId')
         except:
             return HttpResponse(status=400)
 
@@ -108,8 +106,8 @@ class RoomList(View):
     # 获取聊天室列表
 
     def post(self, request: HttpRequest):
-        json_obj = json.loads(request.body)
-        userId = json_obj['userId']
+
+        userId = request.POST.get('userId')
         try:
             user = User.objects.get(id=userId)
         except:
@@ -127,8 +125,9 @@ class RoomList(View):
 
 class FileView(View):
     def post(self, request: HttpRequest):
+
         try:
-            messageId = request.GET['messageId']
+            messageId = request.POST.get('roomId')
         except:
             return HttpResponse(status=400)
         message = ChatMessage.objects.get(id=messageId)
@@ -156,13 +155,12 @@ class FileView(View):
 
 class DocView(View):
     def post(self, request: HttpRequest):
-        json_obj = json.loads(request.body)
         # 检验字段是否完整
         try:
-            title = json_obj['title']
-            link = json_obj['link']
-            roomId = json_obj['roomId']
-            userId = json_obj['userId']
+            title = request.POST.get('title')
+            link = request.POST.get('link')
+            roomId = request.POST.get('roomId')
+            userId = request.POST.get('userId')
         except:
             return HttpResponse({"status":400})
 
@@ -208,7 +206,7 @@ class DocListView(View):
     def post(self, request: HttpRequest):
         # 检验字段是否完整
         try:
-            roomId = request.GET['roomId']
+            roomId = request.POST.get('roomId')
         except:
             return HttpResponse({"status":400})
 
