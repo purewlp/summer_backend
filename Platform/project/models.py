5d1fd5af5ceb915992b31abd3006d29e911aad49
whytomato <1158152324@qq.com>
2023-08-30 20:45:15 +0800

    项目重构

--- Platform/project/models.py
+++ Platform/project/models.py
@@ -32,11 +32,12 @@ class Project(models.Model):
         db_table='project'
 
 class ProjectRecycleBin(models.Model):
+    project=models.OneToOneField(Project,on_delete=models.CASCADE,primary_key=True,default=0)
     name=models.CharField(max_length=100)
     deleter_id=models.BigIntegerField(null=True)
     creator_id=models.BigIntegerField(null=True)
     team=models.ForeignKey(Team,on_delete=models.CASCADE,null=True)
-    project_id=models.BigIntegerField(null=True)
+    # project_id=models.BigIntegerField(null=True)
     created_time=models.DateTimeField(null=True)
     finished=models.BooleanField(default=False)
     finished_time=models.DateTimeField(null=True,blank=True)
@@ -48,12 +49,6 @@ class ProjectRecycleBin(models.Model):
             self.deleted_time = timezone.now()
         if not self.expiration_time:
             self.expiration_time = self.deleted_time + timezone.timedelta(minutes=10)
-        if self.id is None:
-            last_project = ProjectRecycleBin.objects.order_by('-id').first()
-            if last_project:
-                self.id = last_project.id + 1
-            else:
-                self.id = 1
         super().save(*args,**kwargs)
 
     def __str__(self):
@@ -62,17 +57,20 @@ class ProjectRecycleBin(models.Model):
         db_table='project_recyclebin'
 
 class Collection(models.Model):
-    team=models.ForeignKey(Team,on_delete=models.CASCADE)
-    project_id=models.BigIntegerField(null=True)
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    def save(self, *args, **kwargs):
-        # 如果项目ID尚未分配，分配一个从10001开始的唯一项目ID
-        if self.id is None:
-            last_project = Collection.objects.order_by('-id').first()
-            if last_project:
-                self.id = last_project.id + 1
-            else:
-                self.id = 1
-        super().save(*args,**kwargs)
-
+    team=models.OneToOneField(Team,on_delete=models.CASCADE,primary_key=True)
+    project=models.OneToOneField(Project,on_delete=models.CASCADE)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    # def save(self, *args, **kwargs):
+    #     # 如果项目ID尚未分配，分配一个从10001开始的唯一项目ID
+    #     if self.id is None:
+    #         last_project = Collection.objects.order_by('-id').first()
+    #         if last_project:
+    #             self.id = last_project.id + 1
+    #         else:
+    #             self.id = 1
+    #     super().save(*args,**kwargs)
+    class Meta:
+        managed=False
+        db_table='project_collection'
+        unique_together = (("team", "project","user"))
 
