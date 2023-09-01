3a2d874acaf7c7b39b9eee7e9ec97dd415778855
MrYoung-2021 <MrYoung2021@outlook.com>
2023-09-01 11:21:24 +0800

    原型设计同步修改

--- Platform/prototype/customers.py
+++ Platform/prototype/customers.py
@@ -2,7 +2,9 @@ import json
 from urllib.parse import parse_qs
 
 from asgiref.sync import async_to_sync
+from channels.exceptions import StopConsumer
 from channels.generic.websocket import WebsocketConsumer
+
 
 from message.models import UserMessage
 from project.models import Collection
@@ -18,22 +20,23 @@ connect_list = {}
 
 class PrototypeConsumer(WebsocketConsumer):
 
-    def connect(self):
+    def websocket_connect(self, message):
         # 将原型与用户连接，判断用户是否属于原型所属项目所属的team
         query_string = self.scope['query_string']
         query_params = parse_qs(query_string)
         prototypeId = query_params.get(b'prototypeId', [b''])[0].decode().split()[0]
         userId = query_params.get(b'userId', [b''])[0].decode().split()[0]
         user = User.objects.get(id=userId)
-        prototype = Prototype(id=prototypeId)
-        project_prototype = ProjectPrototype.objects.get(id=prototypeId)
+        prototype = Prototype.objects.get(id=prototypeId)
+        project_prototype = ProjectPrototype.objects.get(prototype=prototype)
         project = project_prototype.project
-        project_collection = Collection.objects.get(project=project)
-        team = project_collection.team
+        team = project.team
         try:
             membership = Membership.objects.get(user=user, team=team)
         except:
             return
+
+        # print(self)
 
         if prototypeId not in connect_list:
             connect_list[prototypeId] = []
@@ -43,18 +46,63 @@ class PrototypeConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_add)(self.room_group_name, self.channel_name)
         self.accept()
 
-        componentData = prototype.componentData
-        canvasStyleData = prototype.canvasStyleData
 
-        self.send(json.dumps(componentData))
-        self.send(json.dumps(canvasStyleData))
 
-    def disconnect(self, code):
+        # componentData = prototype.componentData
+        # canvasStyleData = prototype.canvasStyleData
+
+        # prototypes = {
+        #     "componentData": componentData,
+        #     "canvasStyleData": canvasStyleData
+        # }
+
+        # self.send(json.dumps(prototypes))
+
+    def websocket_disconnect(self, message):
+        for connect in connect_list:
+            if str(self) in connect:
+                connect.remove(self)
+                break
         async_to_sync(self.channel_layer.group_discard)(self.room_group_name, self.channel_name)
+        self.disconnect(message['code'])
+        raise StopConsumer()
 
     def websocket_receive(self, message):
-        message = message['message']
-        userId = message['userId']
+        query_string = self.scope['query_string']
+        query_params = parse_qs(query_string)
+        prototypeId = query_params.get(b'prototypeId', [b''])[0].decode().split()[0]
+        userId = query_params.get(b'userId', [b''])[0].decode().split()[0]
+
+        # print(message)
+
+
+        # dic = json.loads(message['text'])
+        cid = message['text']
+        cids = cid.split('$$$')
+        componentData = cids[0]
+        canvasStyleData = cids[1]
+        # cid = json.loads(cid)
+
+        print(componentData)
+        print(canvasStyleData)
+
+
+
+        prototype = Prototype.objects.get(id=prototypeId)
+        prototype.componentData = componentData
+        prototype.canvasStyleData = canvasStyleData
+        prototype.save()
+
+        for connect in connect_list[prototypeId]:
+            prototypes = {
+                "componentData": prototype.componentData,
+                "canvasStyleData": prototype.canvasStyleData
+            }
+
+            if connect == self:
+                continue
+            connect.send(cid)
+
 
         # self.send(json.dumps())
 
